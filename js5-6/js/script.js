641b79093908d6deeae0f2e220cff95832e7f9e1
nesterenko-marina <mary_daydreamer@mail.ru>
2016-05-06 19:18:31 +0300

    Added files via upload

---
+++ js5-6/js/script.js
@@ -0,0 +1,45 @@
+var time = 0;
+var running = 0;
+
+function startPause() {
+    if (running == 0) {
+        running = 1;
+        increment(); document.getElementById("startPause").innerHTML = "Pause";
+    } else {
+        running = 0;        document.getElementById("startPause").innerHTML = "Resume";
+    }
+}
+
+function reset() {
+    running = 0;
+    time = 0;
+    document. getElementById("startPause").innerHTML = "Start";
+    document. getElementById("output").innerHTML = "00:00:00:00";
+}
+
+function increment() {
+    if (running == 1) {
+        setTimeout (function() {
+            time++;
+            var mins = Math.floor(time/10/60);
+            var secs = Math.floor(time/10 % 60);
+            var hours = Math.floor(time/10/60/60);
+            var tenths = time % 100;
+            
+            if (hours < 10) {
+                hours = "0" + hours;
+            }
+            if (mins < 10) {
+                mins = "0" + mins;
+            }
+            if (secs < 10) {
+                secs = "0" + secs;
+            }
+            if (tenths < 10) {
+                tenths = "0" + tenths;
+            }
+            document.getElementById("output").innerHTML = hours + ":" + mins + ":" + secs + ":" + tenths;
+            increment();
+        },100);
+    }
+}
